698cb91905775cd8b239808919fc64d8b2e3128e
Ray Strode <rstrode@redhat.com>
2026-09-15 11:27:55 -0400

    drm: Generate native color-operation bindings
    The kernel defines native objects for per-plane color processing.
    
    Rust plane setup and atomic access lack bindings for those objects.
    
    This commit includes the native color-operation declarations in the
    generated bindings.
    
    Signed-off-by: Ray Strode <rstrode@redhat.com>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -56,6 +56,7 @@
 #include <drm/drm_capture_grant.h>
 #include <drm/clients/drm_client_setup.h>
 #include <drm/drm_client.h>
+#include <drm/drm_colorop.h>
 #include <drm/drm_connector.h>
 #include <drm/drm_crtc.h>
 #include <drm/drm_damage_helper.h>
